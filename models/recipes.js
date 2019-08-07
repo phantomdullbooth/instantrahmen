a1ba354625a609255dbcf6dfff9dc40527afb4a7
A Winslow Mays <49768250+winslowdev@users.noreply.github.com>
2019-08-07 09:58:16 -0400

    final push

--- models/recipes.js
+++ models/recipes.js
@@ -3,8 +3,13 @@ const mongoose = require('mongoose'); // require mongoose package
 const recipeSchema = new mongoose.Schema({
   title: String,
   description: String,
-  code: Array,
-  image: String
+  image: String,
+  line_1: { type: String, required: true},
+  line_2: String,
+  line_3: String,
+  line_4: String,
+  line_5: String,
+  line_6: String,
 })
 
 const Recipes = mongoose.model('Recipes', recipeSchema) // will be stored in 'plants' collection
